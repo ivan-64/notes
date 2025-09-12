14cad9271e888fc07e0e2e5d3d5fc03ce1be0c17
Тугужеков Иван Иванович <ivan.taim@hotmail.com>
2025-09-12 21:55:27 +0200

    finish

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const inputElement = document.getElementById("title");
 const createBtn = document.getElementById("create");
 const listElement = document.getElementById("list");
 
-const notes = ["записать блок про массивы", "теория объектов"];
+
+const notes = [
+  {
+    title: "записать блок про массивы",
+    completed: false,
+  },
+  {
+    title: "теория объектов",
+    completed: true,
+  },
+];
 
 function render() {
-
-    // for (let i = 0; i < notes.length; i++) {
-    //       listElement.insertAdjacentHTML(
-    //         "beforeend",
-    //         getNoteTemplate(notes[i])
-    //       );
-
-    // }
-
-    for (let note of notes) {
-             listElement.insertAdjacentHTML(
-              "beforeend",
-               getNoteTemplate(note))
-    }
-
+  listElement.innerHTML = "";
+  if (notes.length === 0) {
+    listElement.innerHTML = '<p>Заметок нет</p>'
+  }
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i));
+  }
 }
 
 render();
@@ -29,21 +31,44 @@ createBtn.onclick = function () {
     return;
   }
 
-  listElement.insertAdjacentHTML(
-    "beforeend",
-    getNoteTemplate(inputElement.value)
-  );
+  const newNote = {
+    title: inputElement.value,
+    completed: false,
+  };
+
+  notes.push(newNote);
+  render();
   inputElement.value = "";
 };
 
-function getNoteTemplate (title) {
-    return     `           <li class="list-group-item d-flex justify-content-between align-items-center">
-                <span>${title}</span>
+listElement.onclick = function (event) {
+if ((event.target.dataset.index)) {
+  const index = Number(event.target.dataset.index);
+  const type = event.target.dataset.type
+
+  if (type === 'toggle') {
+    notes[index].completed = !notes[index].completed;
+  } else if ('remove') {
+    notes.splice(index, 1)
+  }
+  render()
+}
+}
+
+function getNoteTemplate(note, index) {
+  return `           <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span class="${
+                  note.completed ? "text-decoration-line-through" : ""
+                }">${note.title}</span>
                 <span>
-                    <span class="btn btn-small btn-success">&#10003;</span>
-                    <span class="btn btn-small btn-danger">&#215;</span>
+                    <span class="btn btn-small btn-${
+                      note.completed ? "warning" : "success"
+                    }" data-index="${index}" data-type="toggle">&#10003;</span>
+                    <span class="btn btn-small btn-danger"  data-type="remove" data-index="${index}">&#215;</span>
 
                 </span>
             </li>
-`
+`;
 }
+
+
